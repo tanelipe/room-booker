ef8d1e5c644d1e4357a443b26f8d892e0ce088fa
Taneli Peltoniemi <taneli.peltoniemi@gmail.com>
2018-06-16 17:10:36 +0300

    Updating the routing system and started working on register/login pages

--- frontend/main/src/App.js
+++ frontend/main/src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import { Switch } from 'react-router'
 import { BrowserRouter, Route} from 'react-router-dom';
 
-import Home from './components/home'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+
+import HomePage from './components/home'
+import SignupPage from './components/signup'
+import LoginPage from './components/login'
+import PageNotFound from './components/404'
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark'
+  } 
+})
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <BrowserRouter>
-          <Switch>
-            <Route path='/' component={Home} />
-          </Switch>
-        </BrowserRouter>
+      <MuiThemeProvider theme={theme}>
+        <div>
+          <BrowserRouter>
+            <Switch>
+              <Route path='/' component={HomePage} exact={true} />
+              <Route path='/signup' component={SignupPage} />
+              <Route path='/login' component={LoginPage} />
+              <Route component={PageNotFound} />
+            </Switch>
+          </BrowserRouter>
+        </div>
       </MuiThemeProvider>
     )
   }
--- frontend/main/src/components/home.js
+++ frontend/main/src/components/home.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react'
-import { Card, CardTitle } from 'material-ui/Card'
+import React, { Component } from 'react'
+import NavigationBar from './navigation/index'
 
-class Home extends Component {
+class HomePage extends Component {
   render() {
     return (
-      <Card className="container">
-        <CardTitle title="Room Booking" subtitle="Home page" />
-      </Card>
+      <div>
+        <NavigationBar />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu urna tincidunt, luctus ante ut, ultrices quam. Etiam lorem leo, malesuada non massa et, congue imperdiet quam. Phasellus euismod, augue in consequat ultricies, neque turpis congue enim, vel pellentesque purus magna sit amet turpis. Vestibulum aliquet magna velit, vitae aliquet arcu scelerisque ultrices. Vivamus eget pharetra ante, at porttitor libero. Curabitur et eros metus. Sed aliquet, nisl nec suscipit finibus, dui leo vulputate eros, commodo auctor augue magna a magna. Praesent ac ligula vitae arcu interdum interdum. Quisque ex neque, tincidunt in vehicula et, congue vitae est. Pellentesque lacus tortor, facilisis eu est sed, iaculis elementum felis. Mauris ullamcorper nisi vel massa vestibulum, nec finibus dui tincidunt. Donec bibendum suscipit imperdiet.</p>
+        <p>Praefsent condimentum non leo nec consequat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut imperdiet neque eros, sit amet volutpat lectus egestas ac. Vestibulum tincidunt magna lacus, sit amet faucibus tortor fermentum ut. Proin auctor elit dui, ut consequat magna luctus nec. Cras quis purus lacus. Vivamus blandit cursus tortor, vel finibus nulla consectetur sit amet. Cras in urna lorem. Ut ullamcorper ipsum enim, id bibendum ligula suscipit a. Maecenas consectetur volutpat magna vel bibendum. Nulla id ultricies enim, in eleifend libero. Donec ultricies elementum dapibus. Morbi vulputate augue at magna scelerisque vehicula. Sed at felis in dolor lacinia rhoncus. Curabitur ultricies ante ante. Nulla laoreet mauris vitae imperdiet porttitor.</p>
+      </div>
     )
   }
 }
-
-export default Home
+export default HomePage
